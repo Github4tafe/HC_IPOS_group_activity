017f885e25f21c17327f2074f230376af76e6e93
Hui Cao <00096057@uwa.edu.au>
2025-04-25 00:13:17 +0800

    fix the covert function in the functions.py

--- functions.py
+++ functions.py
@@ -8,4 +8,6 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    if not isinstance(fahrenheit, (int, float)):
+        raise TypeError("Input must be a number")
     return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
